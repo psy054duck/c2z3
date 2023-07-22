71de9a336dfc117a037dae330dfb8016a0cb2629
conditional-recurrence-solver-team <conditional.solver@gmail.com>
2023-07-22 23:19:19 +0800

    sync

--- include/rec_solver.h
+++ include/rec_solver.h
@@ -6,12 +6,12 @@
 class rec_solver {
     private:
         z3::context& z3ctx;
-        z3::expr_vector rec_eqs;
+        std::map<z3::expr, z3::expr> rec_eqs;
         std::map<z3::expr, z3::expr> res;
     public:
-        rec_solver(z3::expr_vector& rec_eqs, z3::context& z3ctx): rec_eqs(rec_eqs), z3ctx(z3ctx) {}
-        rec_solver(z3::context& z3ctx): z3ctx(z3ctx), rec_eqs(z3ctx) {}
-        void set_eqs(z3::expr_vector& rec_eqs);
-        void solve();
+        rec_solver(std::map<z3::expr, z3::expr>& rec_eqs, z3::context& z3ctx);
+        rec_solver(z3::context& z3ctx): z3ctx(z3ctx) {}
+        void set_eqs(std::map<z3::expr, z3::expr>& rec_eqs);
+        void simple_solve();
 };
 #endif
--- src/rec_solver.cpp
+++ src/rec_solver.cpp
@@ -1,1 +1,22 @@
 #include "rec_solver.h"
+
+bool is_simple_rec(z3::func_decl func_decl, z3::expr rhs) {
+    
+    std::all_of(rhs.args().begin(), )
+}
+
+rec_solver::rec_solver(std::map<z3::expr, z3::expr>& eqs, z3::context& z3ctx): z3ctx(z3ctx) {
+    set_eqs(eqs);
+}
+
+void rec_solver::set_eqs(std::map<z3::expr, z3::expr>& eqs) {
+    rec_eqs = eqs;
+}
+
+void rec_solver::simple_solve() {
+    for (auto& func_eq : rec_eqs) {
+        z3::expr func = func_eq.first;
+        z3::expr eq = func_eq.second;
+        func.decl().kind() == Z3_OP_ADD
+    }
+}
